b34ecb3386d87fc08586523c085788c554479538
Joel Eche <joel.eche5@gmail.com>
2020-06-24 19:31:53 -0500

    Routes - headeras component

--- components/LateralMenu.js
+++ components/LateralMenu.js
@@ -2,21 +2,21 @@ const template_lateral_menu = document.createElement("template");
 
 const returnMenuStructure = (activeItem) => {
   return `
-    <link rel="stylesheet" href="./assets/styles/menu.css">
+    <link rel="stylesheet" href="./assets/styles/lateral-menu.css">
     <aside class="lateral-section">
       <nav>
         <ul class="menu list-undecorated">
           <li class="menu__item ${activeItem === "index" ? "menu-active" : ""}">
-            <span>My boards</span>
+            <a class="menu__item__link" href="index.html">My boards</a>
           </li>
           <li class="menu__item ${activeItem === "closed" ? "menu-active" : ""}">
-            <span>Closed boards</span>
-          </li class="menu__item">
+            <a class="menu__item__link" href="closed-boards.html">Closed boards</a>
+          </li>
           <li class="menu__item ${activeItem === "profile" ? "menu-active" : ""}">
-            <span>My profile</span>
+            <a class="menu__item__link" href="profile.html">My profile</a>
           </li>
           <li class="menu__item">
-            Log out
+            <button class="menu__item__link">Log out</button>
           </li>
         </ul>
       </nav>
