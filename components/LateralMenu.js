9c55bc906664c6e64fabadac8d7f8043b6ffe5a5
fernandinux <fernando.paca@gmail.com>
2020-06-26 20:53:29 -0500

    stylos sidebar y boton logout

--- components/LateralMenu.js
+++ components/LateralMenu.js
@@ -9,29 +9,57 @@ const returnMenuStructure = (activeItem) => {
           <li class="menu__item ${activeItem === "index" ? "menu-active" : ""}">
             <a class="menu__item__link" href="index.html">My boards</a>
           </li>
-          <li class="menu__item ${activeItem === "closed" ? "menu-active" : ""}">
+          <li class="menu__item ${
+            activeItem === "closed" ? "menu-active" : ""
+          }">
             <a class="menu__item__link" href="closed-boards.html">Closed boards</a>
           </li>
-          <li class="menu__item ${activeItem === "profile" ? "menu-active" : ""}">
+          <li class="menu__item ${
+            activeItem === "profile" ? "menu-active" : ""
+          }">
             <a class="menu__item__link" href="profile.html">My profile</a>
           </li>
           <li class="menu__item">
-            <button id="logout" class="menu__item__link">Log out</button>
+            <a id="logout" class="menu__item__link href="profile.html"">Log out</a>
           </li>
         </ul>
       </nav>
     </aside>
     `;
-}
+};
 
 class LateralMenu extends HTMLElement {
-
   constructor() {
     super();
     const activeItem = this.getAttribute("active-item");
     template_lateral_menu.innerHTML = returnMenuStructure(activeItem);
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template_lateral_menu.content.cloneNode(true));
+  }
+  connectedCallback() {
+    this.shadowRoot
+      .getElementById("logout")
+      .addEventListener("click", (event) => {
+        logout();
+      });
+  }
+
+  logout() {
+    const dataUser = JSON.parse(localStorage.getItem("userStrg"));
+    console.log(dataUser);
+
+    fetch("http://127.0.0.1:3000/logout/", {
+      method: "POST",
+      headers: {
+        Authorization: `Token token="${dataUser.token}"`,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        localStorage.removeItem("userStrg");
+      })
+
+      .catch((error) => console.log(error));
   }
 }
 
